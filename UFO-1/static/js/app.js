96a9e60293f59de9209d761e8c1a723061ff062b
Kyle O'Malley <kyleomalley@Kyles-MBP-16.lan>
2020-12-18 15:17:26 -0500

    updated java and html

--- UFO-1/static/js/app.js
+++ UFO-1/static/js/app.js
@@ -1,9 +1,41 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-tableData.forEach(function(createTable) {
-    d3.select('table');
-    d3.select("tbody");
-    d3.append('tr')
+// Variables or objects
+let tableBody = d3.select('tbody');
+const columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"];
+let inputFilter = d3.select('#datetime');
+const button = d3.select('#filter-btn');
+
+// Adding data to html as table
+let addData = (createTable) => {
+  createTable.forEach(ufo => {
+      var row = tableBody.append("tr");
+      columns.forEach(column => row.append("td").text(ufo[column])
+      )
+  });
+}
+
+addData(tableData);
+
+// Filter by Date when button clicked
+
+button.on("click", function() {
+  d3.event.preventDefault();
+
+  // make input a value
+  let inputDate = inputFilter.property('value');
+  let filterDatetime = tableData.filter(tableData => tableData.datetime === inputDate);
+
+  tableBody.html("");
+
+  let filterResponse = {
+    filterDatetime
+  }
+  if (filterResponse.filterDatetime.length !== 0) {
+    addData(filterDatetime);
+  }
+  else {
+    tableBody.append("tr").text("No sightings on this date")
+  }
 });
--- UFO-2/static/js/app.js
+++ UFO-2/static/js/app.js
@@ -1,4 +1,29 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Variables or objects
+let tableBody = d3.select('tbody');
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"];
+let inputFilterDate = d3.select('#datetime');
+let inputFilterCity = d3.select('#city');
+let inputFilterState = d3.select('#state');
+let inputFilterCountry = d3.select('#country');
+let inputFilterShape = d3.select('#shape');
+const button = d3.select('#filter-btn');
+
+// Adding data to html as table
+let addData = (createTable) => {
+  createTable.forEach(ufo => {
+      var row = tableBody.append("tr");
+      columns.forEach(column => row.append("td").text(ufo[column])
+      )
+  });
+}
+
+addData(tableData);
+
+button.on("click", function() {
+    d3.event.preventDefault();
+
+
+});
